1f3d8530177fb51be5550c9db37d0d3927e214ab
coderdal <agircoder@gmail.com>
2022-06-29 17:32:12 +0300

    Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
-  return <div>Google UI</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
