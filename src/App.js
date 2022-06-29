c63935cfc18703aba623f825039ae80476c23229
coderdal <agircoder@gmail.com>
2022-06-29 19:35:13 +0300

    Footer design

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 import styles from "./App.module.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <main className={styles.main}>
       <Header />
       <Main />
+      <Footer />
     </main>
   );
 };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,69 @@
 import React from "react";
 
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.top}>Turkey</div>
+
+      <div className={styles.bottom}>
+        <div className={styles.left}>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            About
+          </a>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Advertising
+          </a>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Business
+          </a>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            How Search works
+          </a>
+        </div>
+        <div className={styles.right}>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Privacy
+          </a>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms
+          </a>
+          <a
+            href="https://linkedin.com/in/muhammederdal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
